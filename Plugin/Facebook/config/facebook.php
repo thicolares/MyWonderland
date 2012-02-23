f706eb4af2a99a31f6da827b89ab121c613ba8b2
Thiago Colares <thiago@apimenti.com.br>
2012-02-22 22:55:09 -0200

    Adding Facebook plugin

---
+++ Plugin/Facebook/config/facebook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/Config/facebook.php
+  */
+  $config = array(
+  	'Facebook' => array(
+  		'appId' => '169435716498123',
+  		'apiKey' => '169435716498123',
+  		'secret' => '9d0314dba021a5f4fac233e524e484cc',
+  		'cookie' => true,
+  		'locale' => 'pt_BR',
+  		)
+  	);
+?>
